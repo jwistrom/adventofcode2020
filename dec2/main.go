88243c93b768af6b592369b00dee4d565bf1bccd
Johan Wistrom <johan.wistrom@pricerunner.com>
2020-12-02 16:45:47 +0100

    finalize day 2

--- dec2/main.go
+++ dec2/main.go
@@ -15,16 +15,30 @@ type characterRange struct {
 func main() {
 	lines := utils.ReadLinesFromFile("passwords_and_policies.txt")
 	
-	meetCount := 0
+	meetCount1 := 0
 	for _, line := range lines {
 		charRange, character, password := parseLine(line)
-		meetsPolicy := passwordMeetsPolicy(character, charRange, password)
-		if meetsPolicy {
-			meetCount++
+		meetsPolicy1 := passwordMeetsPolicy1(character, charRange, password)
+		if meetsPolicy1 {
+			meetCount1++
 		}
 	}
 
-	fmt.Printf("%d passwords meet requirements", meetCount)
+	fmt.Printf("%d passwords meet requirements 1\n", meetCount1)
+
+
+	// Part 2
+
+	meetCount2 := 0
+	for _, line := range lines {
+		charRange, character, password := parseLine(line)
+		meetsPolicy2 := passwordMeetsPolicy2(character, charRange, password)
+		if meetsPolicy2 {
+			meetCount2++
+		}
+	}
+
+	fmt.Printf("%d passwords meet requirements 2", meetCount2)
 
 }
 
@@ -45,7 +59,23 @@ func parseLine(line string) (characterRange, string, string) {
 	
 }
 
-func passwordMeetsPolicy(requiredLetter string, requiredLetterCount characterRange, password string) bool {
+func passwordMeetsPolicy1(requiredLetter string, requiredLetterCount characterRange, password string) bool {
 	count := strings.Count(password, requiredLetter)
 	return count >= requiredLetterCount.min && count <= requiredLetterCount.max
+}
+
+func passwordMeetsPolicy2(requiredLetter string, requiredLetterCount characterRange, password string) bool {
+	if len(password) < requiredLetterCount.max {
+		return false
+	}
+
+	minLetter := string(password[requiredLetterCount.min-1])
+	maxLetter := string(password[requiredLetterCount.max-1])
+
+	valid := (minLetter == requiredLetter && maxLetter != requiredLetter) || (minLetter != requiredLetter && maxLetter == requiredLetter)
+
+	//fmt.Printf("Password is %s. The range is %d-%d. MinLetter is %s. MaxLetter is %s. Required char is %s. The password validity: %t\n", password, requiredLetterCount.min, requiredLetterCount.max, minLetter, maxLetter, requiredLetter, valid)
+
+	return valid
+	
 }
