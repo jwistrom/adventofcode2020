eb93ce2570c32eedf0e36385cbc97a651abd5593
Johan Wiström <johan.wistrom@reforceinternational.com>
2020-12-11 08:05:08 +0100

    day10 complete

--- dec10/main.go
+++ dec10/main.go
@@ -9,7 +9,7 @@ import (
 	"errors"
 )
 
-const fileName = "test_joltage.txt"
+const fileName = "output_joltage.txt"
 
 func main() {
 	adapters := parseInput()
@@ -27,178 +27,40 @@ func main() {
 	fmt.Println(distribution)
 	fmt.Printf("The answer to part 1 is %d\n", distribution[1] * distribution[3])
 
-
-
+	allAdapters := make([]int, len(adaptersWithDevice))
+	copy(allAdapters, adaptersWithDevice)
+	allAdapters = append(allAdapters, 0)
+	sort.Ints(allAdapters)
 	
-	//reverseOrderSort := sort.Reverse(sort.IntSlice(adapters))
-	//sort.Sort(reverseOrderSort)
-	combinations := 1
-	fmt.Println(adapters)
-	findAllCombinations4(adapters, &combinations, 1, 1, len(adapters)-2, -1)
-
-	fmt.Printf("Combinations: %d\n", combinations)
+	combinations := findCombinationsCount(allAdapters)
+	fmt.Printf("The answer to part 2 is %d\n", combinations)
 }
 
-func findAllCombinations4(allAdapters []int, combinations *int, level int, minIndex int, maxIndex int, nextToStart int) {
-	
-	for i := 1 ; i < len(allAdapters)-1 ; i++ {
-		current := allAdapters[i]
-		if nextToStart != -1 && nextToStart != current {
-			 continue
-		} 
-		
+func findCombinationsCount(allAdapters []int) int {
+	combinationCount := map[int]int {0:1}
+	combinationCount[0] = 1
 
-		numberCanBeRemoved := canBeRemoved(allAdapters, current)
-		if numberCanBeRemoved {
-			*combinations = *combinations + 1
-			
-			findAllCombinations4(removeFromSlice(allAdapters, current), combinations, level+1, i, i, allAdapters[i+1])
-		} else if level > 1 {
-			return
-		}
-
+	for _, adapt := range allAdapters {
+		combinationCount[adapt] += combinationCount[adapt-1]
+		combinationCount[adapt] += combinationCount[adapt-2]
+		combinationCount[adapt] += combinationCount[adapt-3]
 	}
 
+	return getLast(combinationCount)
 }
 
-func canBeRemoved(slice []int, value int) bool {
-	for i, number := range slice {
-		if number == value {
-			return slice[i-1] - slice[i+1] <= 3
-		}
+func getLast(m map[int]int) (ret int){
+
+	var keys []int
+	for k := range m {
+		keys = append(keys, k)
 	}
-	panic(errors.New("Could not find number"))
-}
 
-func removeFromSlice(slice []int, value int) (ret []int){
-	for _, number := range slice {
-		if number != value {
-			ret = append(ret, number)
-		}
+	sort.Ints(keys)
+	for _, k := range keys {
+		ret = m[k]
 	}
 	return
-}
-
-func findAllCombinations3(allAdapters []int) {
-	sort.Ints(allAdapters)
-	fmt.Println(allAdapters)
-	
-	var waysToGetToIndex []int
-	for i, targetAdapt := range allAdapters {
-		if i == 0 {
-			waysToGetToIndex = append(waysToGetToIndex, 1)
-			continue
-		}
-
-		var waysToTarget int
-		for j, potentialSourceAdapt := range allAdapters {
-			if j >= i {
-				break
-			}
-
-			isValidSource := targetAdapt - potentialSourceAdapt <= 3
-			if isValidSource {
-				waysToTarget++
-			} else {
-				continue
-			}
-		}
-		waysToGetToIndex = append(waysToGetToIndex, waysToTarget)
-	}
-
-	fmt.Println(waysToGetToIndex)
-	fmt.Printf("Number of combinations are %d\n", multiply(waysToGetToIndex))
-	
-}
-
-func findAllCombinations2(allAdapters []int) {
-	sort.Ints(allAdapters)
-	fmt.Println(allAdapters)
-
-	reverseOrderSort := sort.Reverse(sort.IntSlice(allAdapters))
-	sort.Sort(reverseOrderSort)
-	
-	var waysToGetToIndex []int
-	for i, targetAdapt := range allAdapters {
-		if i == len(allAdapters) - 1 {
-			break
-		}
-
-		var waysToTarget int
-		for j, potentialSourceAdapt := range allAdapters {
-			if j <= i {
-				continue
-			}
-
-			isValidSource := targetAdapt - potentialSourceAdapt <= 3
-			if isValidSource {
-				waysToTarget++
-			} else {
-				break
-			}
-		}
-		waysToGetToIndex = append(waysToGetToIndex, waysToTarget)
-	}
-
-	fmt.Println(waysToGetToIndex)
-	fmt.Printf("Number of combinations are %d\n", multiply(waysToGetToIndex))
-	
-}
-
-func multiply(numbers []int) int {
-	ret := 1
-	for _, number := range numbers {
-		ret = ret * number
-	}
-	return ret
-}
-
-func findAllCombinations(allAdapters []int) {
-	if !sort.IntsAreSorted(allAdapters) {
-		panic(errors.New("Adapters must be sorted"))
-	}
-
-	combinationCount := 0
-	for i, adapt := range allAdapters {
-		if adapt > 3 {
-			break
-		}
-
-		currentAdapters := []int {adapt}
-		findNextAdapter(allAdapters, currentAdapters, i+1, &combinationCount)
-	}
-
-
-	fmt.Printf("Number of combinations are %d\n", combinationCount)
-}
-
-func findNextAdapter(allAdapters []int, currentAdapters []int, startSearchAt int, combinationCount *int) {
-
-	lastAdapter := currentAdapters[len(currentAdapters) - 1]
-
-	for i, adapt := range allAdapters {
-		if i < startSearchAt {
-			continue
-		}
-
-		isValidNext := adapt - lastAdapter <= 3
-
-		if !isValidNext {
-			break
-		}
-
-		if startSearchAt == len(allAdapters) - 1 {
-			*combinationCount = *combinationCount + 1
-			//fmt.Printf("Found combination %v\n", currentAdapters)
-			fmt.Printf("Found combinations %d\n", *combinationCount)
-		}
-
-		newCurrentAdapters := make([]int, len(currentAdapters))
-		copy(newCurrentAdapters, currentAdapters)
-		newCurrentAdapters = append(newCurrentAdapters, adapt)
-
-		findNextAdapter(allAdapters, newCurrentAdapters, i+1, combinationCount)
-	}
 }
 
 func findJoltageDifferenceDistribution(adapters []int) map[int]int {
